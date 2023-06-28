d8a0da8afd203ef78e71da509c56b0ad266ce3b8
br <learnloop365@gmail.com>
2023-06-28 18:18:59 -0400

    init

---
+++ 1_Hello_World/HW.java
@@ -0,0 +1,17 @@
+/*
+ * Compile
+ * e.g.
+ * javac -d build HW.java
+ *
+ * Run
+ * e.g.
+ * cd build
+ * java HW
+ *
+*/
+
+public class HW {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
